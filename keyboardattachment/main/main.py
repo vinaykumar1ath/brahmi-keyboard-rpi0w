a8e767d4d60a8b6cb63d0fce448bf107b3b00d6f
channabassu <vinaykumar1ath@gmail.com>
2024-11-14 06:07:36 +0530

    updated

--- keyboardattachment/main/main.py
+++
@@ -1,6 +0,0 @@
-# from threading import Thread
-from time import sleep
-from modules.readkeyboard import readkeyboardthrd
-# from modules.window import *  
-
-readkeyboardthrd()
--- keyboardattachment/main/modules/debug.py
+++
@@ -1,8 +0,0 @@
-
-def debugconsolemessage(message,newmessage=True):
-    message=str(message)
-    if not newmessage:
-        print(' ')
-    else:
-        print('\n')
-    print(message)
